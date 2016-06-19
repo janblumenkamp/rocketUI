1de328c017be7d6acd932013fbc09abff9f0bf66
Jan Blumenkamp <jan@blumenkamp.net>
2016-06-19 15:53:52 +0200

    Added class to parse config xml

---
+++ flightdataxmlreader.cpp
@@ -0,0 +1,5 @@
+#include "flightdataxmlreader.h"
+
+FlightDataXMLReader::FlightDataXMLReader()
+{
+}
---
+++ flightdataxmlreader.h
@@ -0,0 +1,10 @@
+#ifndef FLIGHTDATAXMLREADER_H
+#define FLIGHTDATAXMLREADER_H
+
+class FlightDataXMLReader
+{
+public:
+    FlightDataXMLReader();
+};
+
+#endif // FLIGHTDATAXMLREADER_H
